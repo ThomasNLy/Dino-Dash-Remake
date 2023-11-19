f052b240b61d35fa06950f48e05a2e2362d9c1c3
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2023-11-18 21:15:12 -0500

    Update SceneLoader.cs

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -11,6 +11,6 @@ public class SceneLoader : MonoBehaviour
     public void LoadScene(string sceneName)
     {
         Debug.Log("Loading Scene: " + sceneName);
-        //SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(sceneName);
     }
 }
