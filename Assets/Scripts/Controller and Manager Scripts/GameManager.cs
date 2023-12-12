8e59db56b48bd948e996c019630396dee2a8c3c9
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2023-12-12 14:13:43 -0500

    Added a scoring system
    The longer you play the game the high your score becomes

--- Assets/Scripts/Controller and Manager Scripts/GameManager.cs
+++ Assets/Scripts/Controller and Manager Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -9,12 +10,20 @@ public class GameManager : MonoBehaviour
     [Header("Points and Gameplay Stats")]
     [SerializeField]
     private int powerUpPoints;
+    [SerializeField]
+    private int score;
+    [SerializeField]
+    private float scoreTimer;
 
     [Header("Floor and obstacle scrolling speed")]
     public int scrollingSpeed;
 
     [Header("Floor parrallax game object")]
     public ScrollingParallax floor;
+
+
+    [Header("Game State")]
+    private bool gamePaused;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -26,19 +35,29 @@ public class GameManager : MonoBehaviour
             Instance = this;
             Time.timeScale = 1.0f;
         }
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
         powerUpPoints = 0;
+        score = 0;
         scrollingSpeed = -5;
+        scoreTimer = 0f;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         floor.ScrollingSpeed = scrollingSpeed;
+        ScoreTimer();
+      
+        UIManager.Instance.UpdateUI();
+     
+       
     }
 
     public int PowerUpPoints
@@ -46,6 +65,18 @@ public class GameManager : MonoBehaviour
         get { return powerUpPoints; }
         set { powerUpPoints = value; }
     }
+    public int Score
+    {
+        get { return score; }
+        set { score = value;  }
+    }
+
+    public bool GamePaused
+    {
+        get { return gamePaused; }
+        set { gamePaused = value; }
+    }
+
 
     public void PauseGame()
     {
@@ -56,4 +87,38 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 1f;
     }
+
+    public void ToggleGamePaused()
+    {
+
+        gamePaused = !gamePaused;
+        if (gamePaused)
+        {
+            Debug.Log("PauseGame");
+            PauseGame();
+          
+        }
+        else
+        {
+            ResumeGame();
+        }
+        Debug.Log($"Game manager is paused: {gamePaused}");
+        UIManager.Instance.ShowPauseMenu(gamePaused);
+    }
+
+    private void ScoreTimer()
+    {
+        if (scoreTimer < 3f)
+        {
+            scoreTimer += Time.deltaTime;
+           
+        }
+        else
+        {
+            scoreTimer = 0f;
+            score += 1;
+        }
+    }
+ 
+    
 }
