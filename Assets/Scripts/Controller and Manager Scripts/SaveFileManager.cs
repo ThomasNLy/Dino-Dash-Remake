690b71ac964b5e0d6fe8cba2a09820f64901fb13
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2024-01-04 21:36:54 -0500

    Update SaveFileManager.cs
    Can now save data to json format

--- Assets/Scripts/Controller and Manager Scripts/SaveFileManager.cs
+++ Assets/Scripts/Controller and Manager Scripts/SaveFileManager.cs
@@ -1,13 +1,17 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using Utility;
 
+
 public class SaveFileManager : MonoBehaviour
 {
-    SaveDataInfo saveDataInfo = new SaveDataInfo();
+ 
     public static SaveFileManager Instance;
+   
+
 
 
     private void Awake()
@@ -19,49 +23,124 @@ public class SaveFileManager : MonoBehaviour
         else
         {
             Instance = this;
+            LoadSaveData();
+
+           // LoadSaveData2();
+           
+
         }
-        saveDataInfo.score = Utility.SaveData.score;
-        saveDataInfo.highScore = Utility.SaveData.highScore;
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
-        Debug.Log(saveDataInfo.highScore);
+      
+       
     }
 
 
     public void SaveScore(int score)
     {
-        Utility.SaveData.score = score;
-        saveDataInfo.score = score;
-        if (score > saveDataInfo.highScore)
+   
+        if (score > Utility.SaveData.highScore)
         {
             Utility.SaveData.highScore = score;
-            saveDataInfo.highScore = score;
+           
         }
-        string jsonString = JsonUtility.ToJson(saveDataInfo);
-        Debug.Log(jsonString);
+
+        LevelSaveDataInfo data = new LevelSaveDataInfo("Level1", Utility.SaveData.highScore);
+        string jsonString = JsonUtility.ToJson(data);
+        string saveFile = Application.persistentDataPath + "/SaveFile.json";
+        File.WriteAllText(saveFile, jsonString);
+
+       
 
 
     }
 
-    public int GetScore()
+    public void SaveScore2(int score)
     {
-        return saveDataInfo.score;
+        if (score > Utility.SaveData.highScore)
+        {
+            Utility.SaveData.highScore = score;
+
+        }
+
+        LevelSaveDataInfo data = new LevelSaveDataInfo("Level1", Utility.SaveData.highScore);
+        SaveFileMultiLevels datafile = new SaveFileMultiLevels(1, new[] { data});
+        string jsonString = JsonUtility.ToJson(datafile);
+        string saveFile = Application.persistentDataPath + "/SaveFile.json";
+        File.WriteAllText(saveFile, jsonString);
     }
+
+  
     public int GetHighScore()
     {
-        return saveDataInfo.highScore;
+        return Utility.SaveData.highScore;
     }
 
-    public SaveDataInfo GetSaveData()
+    private void LoadSaveData()
     {
-        return saveDataInfo;
+        string saveFile = Application.persistentDataPath + "/SaveFile.json";
+        try
+        {
+            if (File.Exists(saveFile))
+            {
+                LevelSaveDataInfo saveDataInfo = JsonUtility.FromJson<LevelSaveDataInfo>(File.ReadAllText(saveFile));
+                Utility.SaveData.highScore = saveDataInfo.highscore;
+               
+            }
+            else
+            {
+             
+                Utility.SaveData.highScore = 0;
+                LevelSaveDataInfo data = new LevelSaveDataInfo("level1", Utility.SaveData.highScore);
+                string jsonString = JsonUtility.ToJson(data);
+                File.WriteAllText(saveFile, jsonString);
+
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+
+    /**
+     * for usage when having multiple levels/ different levels in the game in the future
+     */
+    private void LoadSaveData2()
+    {
+        string saveFile = Application.persistentDataPath + "/SaveFile2.json";
+        try
+        {
+            if (File.Exists(saveFile))
+            {
+                SaveFileMultiLevels saveDataInfo = JsonUtility.FromJson<SaveFileMultiLevels>(File.ReadAllText(saveFile));
+                Debug.Log(saveDataInfo.levels[0]);
+                LevelSaveDataInfo d = JsonUtility.FromJson<LevelSaveDataInfo>(saveDataInfo.levels[0]);
+              
+            }
+            else
+            {
+
+
+                SaveFileMultiLevels data = new SaveFileMultiLevels(1, new[] { new LevelSaveDataInfo("Level1", 0) });
+                string jsonString = JsonUtility.ToJson(data);
+                File.WriteAllText(saveFile, jsonString);
+
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.LogError(e.Message);
+        }
     }
 
 
 
-    
+
+
+
 }
