a2ff6712ae3d6d0a7a00cefa298d9af1035772c1
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2024-01-04 21:36:30 -0500

    Update Utility.cs

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -21,14 +21,62 @@ public static class AnimationStates
 namespace Utility {
     public static class SaveData
     {
-        public static int score;
+  
         public static int highScore;
     }
-
+    public static class SettingsData
+    {
+        public static float soundEffectVol;
+        public static float bgMusicVol;
+    }
+    public struct SettingsSaveData
+    {
+        public float soundEffectVol;
+        public float bgMusicVol;
+    }
     public struct SaveDataInfo
     {
-        public int score;
-        public int highScore;
+        public int highscore;
+      
+        public SaveDataInfo(int highscore)
+        {
+            
+            this.highscore = highscore;
+         
+        }
+
     }
+
+    /**
+     * 
+     * code below for future use when having multiple levels
+     * 
+     */
+    public struct LevelSaveDataInfo
+    {
+        public string levelName;
+        public int highscore;
+        public LevelSaveDataInfo(string lvName, int score)
+        {
+            this.levelName = lvName;
+            this.highscore = score;
+        }
+
+    }
+    public struct SaveFileMultiLevels
+    {
+        public string[] levels;
+        public SaveFileMultiLevels(int numLvls, LevelSaveDataInfo[] jsonData)
+        {
+            levels = new string[numLvls];
+            for (int i = 0; i < numLvls; i++)
+            {
+                levels[i] = JsonUtility.ToJson(jsonData[i]);
+            }
+
+        }
+        
+    }
+
 }
 
