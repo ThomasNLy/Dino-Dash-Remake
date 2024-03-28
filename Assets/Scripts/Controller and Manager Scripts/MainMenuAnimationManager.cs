2bd4e3aee85571864f2538b9c09384e95eecc819
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2024-03-28 14:21:17 -0400

    Changed script to allow the player Dino to run behind the bush when running and hide behind when popping up

--- Assets/Scripts/Controller and Manager Scripts/MainMenuAnimationManager.cs
+++ Assets/Scripts/Controller and Manager Scripts/MainMenuAnimationManager.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class MainMenuAnimationManager : MonoBehaviour
 {
     public Animator dinoAnimator;
-   
+    public GameObject behindBush;
+    public GameObject infrontOfBush;
+
     public void RandomizeDinoAnimation()
     {
         int randNum = (int)Random.Range(0, 2);
         dinoAnimator.SetFloat("type", randNum);
 
+    }
+
+    public void RunInFrontOfBush()
+    {
+        this.transform.SetParent(infrontOfBush.transform);
+    }
+    public void HideBehindBush()
+    {
+        this.transform.SetParent(behindBush.transform);
     }
 }
